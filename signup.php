513f59abee0bd27bfa2be63d8683d50c9622b5f8
lucia77kaguya <lucia.77kaguya@gmail.com>
2013-08-19 15:28:41 -0700

    edit index.html, login.php, signup.php, add img..

---
+++ signup.php
@@ -0,0 +1,4 @@
+Welcome to join us, <?php echo $_GET["signup_role"]; ?> <?php echo $_GET["signup_account"]; ?> !<br>
+Your input password is <?php echo $_GET["signup_password"]; ?> ,<br>
+and, your confirm password is <?php echo $_GET["signup_confirm_password"]; ?> .<br>
+Here is your signup email: <?php echo $_GET["signup_email"]; ?>
