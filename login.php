9f691a6ea8abcb5d17d8e8c24d592ac413f460e6
carter213 <passcut213@gmail.com>
2013-08-19 18:34:17 -0700

    fix the layout

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 Account: <?php echo $_POST["login_account"]; ?> <br>
 Password: <?php echo $_POST["login_password"]; ?> <br>
 
-<a href="/profmanagement.html">Professor account</a> <br>
-<a href="/tamanagement.html">TA account</a> <br>
-<a href="/studentmanagement.html">Student account</a>
+<a href="/profmanagement">Professor account</a> <br>
+<a href="/tamanagement">TA account</a> <br>
+<a href="/studentmanagement">Student account</a>
--- signup.php
+++ signup.php
@@ -4,6 +4,6 @@ Confirm password: <?php echo $_POST["signup_confirm_password"]; ?> <br>
 Email: <?php echo $_POST["signup_email"]; ?> <br>
 Role: <?php echo $_POST["signup_role"]; ?> <br>
 
-<a href="/profmanagement.html">Professor account</a> <br>
-<a href="/tamanagement.html">TA account</a> <br>
-<a href="/studentmanagement.html">Student account</a>
+<a href="/profmanagement">Professor account</a> <br>
+<a href="/tamanagement">TA account</a> <br>
+<a href="/studentmanagement">Student account</a>
