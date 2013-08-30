fd09417cd2fb2df9c8ed43587d908d3fcedf83aa
Michael Nguyen <mnguyen5904@yahoo.com>
2013-08-29 23:50:05 -0700

    confirm leave class and quiz select

--- tamanagement.php
+++ tamanagement.php
@@ -21,8 +21,8 @@ $classes = mysqli_query($con, "SELECT * FROM class NATURAL JOIN class_member
   WHERE user_id=${user_id}");
 
 // Retrieve quizzes
-$class_quiz_arrays = mysqli_query($con, "SELECT * FROM class_member NATURAL JOIN
-  quiz WHERE user_id=${user_id}");
+$class_quiz_arrays = mysqli_query($con, "SELECT * FROM class NATURAL JOIN
+  class_member NATURAL JOIN quiz WHERE user_id=${user_id}");
 
 ?>
 
@@ -107,7 +107,8 @@ $class_quiz_arrays = mysqli_query($con, "SELECT * FROM class_member NATURAL JOIN
             <input type="password" class="form-control" 
                    id="password" name="password" placeholder="Enter new password">
             <input type="password" class="form-control"
-                   id="confirm_password" name="confirm_password" placeholder="Confirm Password">
+                   id="confirm_password" name="confirm_password" 
+                   placeholder="Confirm Password">
           </div>
           <button type="submit" class="btn btn-default">Submit</button>
         </form>
@@ -132,7 +133,8 @@ $class_quiz_arrays = mysqli_query($con, "SELECT * FROM class_member NATURAL JOIN
         <form id="class_select_form" action="leave_class" method="post">
           <div class="form-group">
             <label for="classes_dropdown">Classes (changes Quizzes)</label>
-            <select id="classes_dropdown" class="form-control" name="class_select">
+            <select id="classes_dropdown" class="form-control" name="class_select"
+                    onchange="change_class()">
               <?php
               while ($class = mysqli_fetch_array($classes)) {
                 $class_code = $class['class_code'];
@@ -142,17 +144,28 @@ $class_quiz_arrays = mysqli_query($con, "SELECT * FROM class_member NATURAL JOIN
               ?>
             </select>
           </div>
-          <button type="submit" class="btn btn-default">Leave class</button>
+          <button type="button" class="btn btn-default" 
+                  onclick="confirm_leave_class()">Leave class</button>
+          <button id="leave_class_btn" type="submit" style="display:none"
+                  class="btn btn-default">Leave class</button>
         </form>
       </div>
       <div class="span4">
-        <form id="quiz_select_form" action= "./grading">
+        <form id="quiz_select_form" action= "./grading" method='post'>
           <div class="form-group">
             <label for="quizzes_dropdown">Quizzes</label>
             <select id="quizzes_dropdown" class="form-control" name="quiz_select">
-              <option>Quiz 1</option>
-              <option>Quiz 2</option>
+              <option/>
+              <?php
+              while ($class_quiz = mysqli_fetch_array($class_quiz_arrays)) {
+                $class_code = $class_quiz['class_code'];
+                $quiz_name = $class_quiz['quiz_name'];
+                print "<option class='${class_code}'>${quiz_name}</option>";
+              }
+              ?>
             </select>
+            <input id="class_quiz" type="text" style="display:none" 
+                   name="class_quiz"/>
           </div>
           <button type="submit" class="btn btn-default">Grade quiz</button>
         </form>
@@ -197,6 +210,29 @@ $class_quiz_arrays = mysqli_query($con, "SELECT * FROM class_member NATURAL JOIN
     var result = confirm("Are you sure you want to delete your account?");
     if (result == true) {
       document.getElementById("delete_btn").click();
+    }
+  }
+
+  function change_class() {
+    $("#class_quiz").val($("#classes_dropdown").val());
+
+    $("#quizzes_dropdown").val('');
+
+    var class_quiz_options = $("#quizzes_dropdown").children();
+    $.each(class_quiz_options, function() {
+      $(this).hide();
+    });
+
+    var shown_options = $("." + $("#classes_dropdown").val());
+    $.each(shown_options, function() {
+      $(this).show();
+    });
+  }
+
+  function confirm_leave_class() {
+    var result = confirm("Are you sure you want to leave this class?");
+    if (result == true) {
+      document.getElementById("leave_class_btn").click();
     }
   }
 </script>
