f8f51c91e024e7a8e3d7705ebdb948c87e5f97dc
Michael Nguyen <mnguyen5904@yahoo.com>
2013-08-31 13:30:09 -0700

    remove graphics

--- assets/js/application.js
+++ assets/js/application.js
@@ -526,11 +526,13 @@ function getQuestionElements(text) {
 
 // Image upload
 function getFileElement() {
+  return $('<br>');
+  /*
   return $('<label>Optional Graphic</label><div class="fileupload fileupload-new" data-provides="fileupload">'
      + '<div class="fileupload-new thumbnail" style="width: 50px; height: 50px;"><img src="http://www.placehold.it/50x50/AAAAAA/FFFFFF&text=+" /></div>'
      + '<div class="fileupload-preview fileupload-exists thumbnail" style="width: 50px; height: 50px;"></div>'
      + '<span class="btn btn-file"><span class="fileupload-new">Select image</span><span class="fileupload-exists">Change</span><input type="file" /></span>'
-     + '<a href="#" class="btn fileupload-exists" data-dismiss="fileupload">Remove</a></div>');
+     + '<a href="#" class="btn fileupload-exists" data-dismiss="fileupload">Remove</a></div>'); */
 }
 
 // Tour button
