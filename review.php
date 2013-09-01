9868b3a874b75a5c441c6aae620c72bef01f736c
Michael Nguyen <min014@ACS-CSEB220-18.ad.ucsd.edu>
2013-09-01 12:06:24 -0700

    review.php

--- review.php
+++ review.php
@@ -27,61 +27,24 @@ $class_code = mysqli_real_escape_string($con, $class_code);
 $quiz_name = filter_var($_GET['quiz_name'], FILTER_SANITIZE_STRING);
 $quiz_name = mysqli_real_escape_string($con, $quiz_name);
 
-// Check if class and quiz are valid
+// Check if quiz is valid
 if (strlen($class_code) != 40 || strlen($quiz_name) == 0 || strlen($quiz_name) > 40) {
-  if ($role == 'Instructor') {
-    header('Location: profmanagement');
-    exit();
-  } else {
-    header('Location: tamanagement');
-	exit();
-  }
+  header('Location: studentmanagement');
+  exit();
 }
 
-if ($role == 'Instructor') {
-  $valid_class = mysqli_query($con, "SELECT * FROM class NATURAL JOIN quiz WHERE 
-    prof_id=${user_id} AND class_code='${class_code}' AND quiz_name='${quiz_name}'");
-
-  if (mysqli_num_rows($valid_class) != 1) {
-    header('Location: profmanagement');
-    exit();
-  }
-} else {
-  $valid_class = mysqli_query($con, "SELECT * FROM class_member NATURAL JOIN
-    class NATURAL JOIN quiz WHERE user_id=${user_id} AND 
-    class_code='${class_code}' AND quiz_name='${quiz_name}'");
+$valid_class = mysqli_query($con, "SELECT * FROM class_member NATURAL JOIN
+  class NATURAL JOIN quiz NATURAL JOIN student_quiz WHERE user_id=${user_id} AND 
+  class_code='${class_code}' AND quiz_name='${quiz_name}' AND graded=1");
 	
-  if (mysqli_num_rows($valid_class) != 1) {
-    header('Location: tamanagement');
-    exit();
-  }
+if (mysqli_num_rows($valid_class) != 1) {
+  header('Location: studentmanagement');
+  exit();
 }
 
-// Check if specific student requested
-if (isset($_GET['student_name'])) {
-  $load_student_name = $_GET['student_name'];
+$valid_class = mysqli_fetch_array($valid_class);
 
-  $valid_quiz = mysqli_query($con, "SELECT * FROM quiz NATURAL JOIN class 
-    NATURAL JOIN student_quiz NATURAL JOIN user WHERE 
-    class_code='${class_code}' AND quiz_name='${quiz_name}' 
-    AND account_name='${load_student_name}'");
-
-  if (mysqli_num_rows($valid_quiz) == 1) {
-    $valid_quiz = mysqli_fetch_array($valid_quiz);
-    $load_quiz = 1;
-    $load_quiz_id = $valid_quiz['quiz_id'];
-    $student_id = $valid_quiz['user_id'];
-  } else {
-    $load_quiz = 0;
-  }
-} else {
-  $load_quiz = 0;
-}
-
-// Get students
-$students = mysqli_query($con, "SELECT * FROM quiz NATURAL JOIN class 
-  NATURAL JOIN class_member NATURAL JOIN student_quiz NATURAL JOIN user WHERE
-  class_code='${class_code}' AND quiz_name='${quiz_name}' AND finished=1");
+$quiz_id = $valid_class['quiz_id'];
 
 ?>
 
@@ -91,7 +54,7 @@ $students = mysqli_query($con, "SELECT * FROM quiz NATURAL JOIN class
 ================================================== -->
 <head>
 <meta charset="utf-8">
-<title>Quiz Grading</title>
+<title>Quiz Review</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="">
 <meta name="author" content="">
@@ -141,13 +104,7 @@ legend + .qtitle:nth-of-type(1) {
         <ul class="nav">
           <li class=""> <a href="./index">Home</a> </li>
           <li class=""> 
-            <?php
-            if ($role == 'Instructor') {
-              print '<a href="./profmanagement" class="nav_links">User Management</a>';
-            } else {
-              print '<a href="./tamanagement" class=nav_links">User Management</a>';
-            }
-            ?>
+			<a href="./studentmanagement" class="nav_links">User Management</a>
           </li>
         </ul>
       </div>
@@ -161,36 +118,21 @@ legend + .qtitle:nth-of-type(1) {
   <div class="row-fluid">
     <div class="span2"> </div>
     <div class="span8">
-      <form action="submit_grading" method="post">
+      <form action="submit_review" method="post">
         <section>
-          <select onchange="load_student_quiz()" name="loadStudentQuiz" id="loadStudentQuiz" class="span2">
-            <?php
-            while ($student = mysqli_fetch_array($students)) {
-              $student_name = $student['account_name'];
-              print "<option value='${student_name}' ";
-              if ($load_quiz == 1) {
-                if ($student_name === $load_student_name) {
-                  print "selected";
-                }
-              }
-              print ">${student_name}</option>";
-            }
-            ?>
-          </select>
           <input id="quiz_name" type="text" style="display:none" name="quiz_name"
                  value=<?php print "'${quiz_name}'" ?>/>
           <input id="class_code" type="text" style="display:none" name="class_code" 
                  value=<?php print "'${class_code}'" ?>/>
         </section>
         <?php
-        // Get questions
-        if ($load_quiz == 1) {
+          // Get questions
           print "<section>";
 
           $questions = mysqli_query($con, "SELECT * FROM quiz NATURAL JOIN question
             NATURAL JOIN student_quiz NATURAL JOIN student_question 
-            NATURAL JOIN user WHERE account_name='${load_student_name}' AND
-            quiz_id=${load_quiz_id} ORDER BY question_num");
+            NATURAL JOIN user WHERE user_id=${user_id} AND
+            quiz_id=${quiz_id} ORDER BY question_num");
 
           while ($question = mysqli_fetch_array($questions)) {
             $label = $question['label'];
@@ -209,7 +151,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "<div class='well well-small'>${body}\n";
 
                 $options = mysqli_query($con, "SELECT * FROM mc
-                  WHERE quiz_id='${load_quiz_id}' AND question_num='${question_num}' 
+                  WHERE quiz_id='${quiz_id}' AND question_num='${question_num}' 
                   ORDER BY option_num");
 
                 while ($option = mysqli_fetch_array($options)) {
@@ -217,7 +159,7 @@ legend + .qtitle:nth-of-type(1) {
                   $option_num = $option['option_num'];
 
                   $student_response = mysqli_query($con, "SELECT * FROM student_mc 
-                    WHERE user_id=${student_id} AND quiz_id=${load_quiz_id} AND 
+                    WHERE user_id=${user_id} AND quiz_id=${quiz_id} AND 
                     question_num=${question_num} AND option_num=${option_num}");
 
                   if (mysqli_num_rows($student_response) > 0) {
@@ -242,7 +184,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Q${question_num} Grade: </label>\n";
                 print "  </div>\n";
                 print "  <div class='input-append'>\n";
-                print "    <select class='span10' name='points[]'>\n";
+                print "    <select class='span10' disabled>\n";
 
                 for ($x = 0; $x < $points; $x++) {
                   print "      <option value='${x}' ";
@@ -259,7 +201,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Q${question_num} comment:</label>\n";
                 print "  </div>\n";
                 print "  <div class='control-group'>\n";
-                print "    <textarea rows='3' name='ta_comment[]'>\n";
+                print "    <textarea rows='3' disabled>\n";
                 print "      ${ta_comment}\n";
                 print "    </textarea>\n";
                 print "  </div>\n";
@@ -267,7 +209,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Student response:</label>\n";
                 print "  </div>\n";
                 print "  <div class='control-group'>\n";
-                print "    <textarea rows='3' disabled>\n";
+                print "    <textarea rows='3' name='student_response[]'>\n";
                 print "      ${student_response}\n";
                 print "    </textarea>\n";
                 print "  </div>\n";
@@ -297,7 +239,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Q${question_num} Grade: </label>\n";
                 print "  </div>\n";
                 print "  <div class='input-append'>\n";
-                print "    <select class='span10' name='points[]'>\n";
+                print "    <select class='span10' disabled>\n";
 
                 for ($x = 0; $x < $points; $x++) {
                   print "      <option value='${x}' ";
@@ -314,7 +256,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Q${question_num} comment:</label>\n";
                 print "  </div>\n";
                 print "  <div class='control-group'>\n";
-                print "    <textarea rows='3' name='ta_comment[]'>\n";
+                print "    <textarea rows='3' disabled>\n";
                 print "      ${ta_comment}\n";
                 print "    </textarea>\n";
                 print "  </div>\n";
@@ -322,7 +264,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Student response:</label>\n";
                 print "  </div>\n";
                 print "  <div class='control-group'>\n";
-                print "    <textarea rows='3' disabled>\n";
+                print "    <textarea rows='3' name='student_response[]'>\n";
                 print "      ${student_response}\n";
                 print "    </textarea>\n";
                 print "  </div>\n";
@@ -336,8 +278,8 @@ legend + .qtitle:nth-of-type(1) {
                 print "<div class='well well-small'>${body}\n";
 
                 $options = mysqli_query($con, "SELECT * FROM student_matching 
-                  NATURAL JOIN matching WHERE quiz_id=${load_quiz_id} AND 
-                  user_id=${student_id} AND question_num=${question_num} 
+                  NATURAL JOIN matching WHERE quiz_id=${quiz_id} AND 
+                  user_id=${user_id} AND question_num=${question_num} 
                   ORDER BY option_num");
 
                 while ($option = mysqli_fetch_array($options)) {
@@ -358,7 +300,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Q${question_num} Grade: </label>\n";
                 print "  </div>\n";
                 print "  <div class='input-append'>\n";
-                print "    <select class='span10' name='points[]'>\n";
+                print "    <select class='span10' disabled>\n";
 
                 for ($x = 0; $x < $points; $x++) {
                   print "      <option value='${x}' ";
@@ -375,7 +317,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Q${question_num} comment:</label>\n";
                 print "  </div>\n";
                 print "  <div class='control-group'>\n";
-                print "    <textarea rows='3' name='ta_comment[]'>\n";
+                print "    <textarea rows='3' disabled>\n";
                 print "      ${ta_comment}\n";
                 print "    </textarea>\n";
                 print "  </div>\n";
@@ -383,7 +325,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Student response:</label>\n";
                 print "  </div>\n";
                 print "  <div class='control-group'>\n";
-                print "    <textarea rows='3' disabled>\n";
+                print "    <textarea rows='3' name='student_response[]'>\n";
                 print "      ${student_response}\n";
                 print "    </textarea>\n";
                 print "  </div>\n";
@@ -402,7 +344,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Q${question_num} Grade: </label>\n";
                 print "  </div>\n";
                 print "  <div class='input-append'>\n";
-                print "    <select class='span10' name='points[]'>\n";
+                print "    <select class='span10' disabled>\n";
 
                 for ($x = 0; $x < $points; $x++) {
                   print "      <option value='${x}' ";
@@ -419,7 +361,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Q${question_num} comment:</label>\n";
                 print "  </div>\n";
                 print "  <div class='control-group'>\n";
-                print "    <textarea rows='3' name='ta_comment[]'>\n";
+                print "    <textarea rows='3' disabled>\n";
                 print "      ${ta_comment}\n";
                 print "    </textarea>\n";
                 print "  </div>\n";
@@ -427,7 +369,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Student response:</label>\n";
                 print "  </div>\n";
                 print "  <div class='control-group'>\n";
-                print "    <textarea rows='3' disabled>\n";
+                print "    <textarea rows='3' name='student_response[]'>\n";
                 print "      ${student_response}\n";
                 print "    </textarea>\n";
                 print "  </div>\n";
@@ -447,7 +389,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Q${question_num} Grade: </label>\n";
                 print "  </div>\n";
                 print "  <div class='input-append'>\n";
-                print "    <select class='span10' name='points[]'>\n";
+                print "    <select class='span10' disabled>\n";
 
                 for ($x = 0; $x < $points; $x++) {
                   print "      <option value='${x}' ";
@@ -464,7 +406,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Q${question_num} comment:</label>\n";
                 print "  </div>\n";
                 print "  <div class='control-group'>\n";
-                print "    <textarea rows='3' name='ta_comment[]'>\n";
+                print "    <textarea rows='3' disabled>\n";
                 print "      ${ta_comment}\n";
                 print "    </textarea>\n";
                 print "  </div>\n";
@@ -472,7 +414,7 @@ legend + .qtitle:nth-of-type(1) {
                 print "    <label>Student response:</label>\n";
                 print "  </div>\n";
                 print "  <div class='control-group'>\n";
-                print "    <textarea rows='3' disabled>\n";
+                print "    <textarea rows='3' name='student_response[]'>\n";
                 print "      ${student_response}\n";
                 print "    </textarea>\n";
                 print "  </div>\n";
@@ -486,13 +428,10 @@ legend + .qtitle:nth-of-type(1) {
           print "</section>";
           print "<div class='form-actions'>\n";
           print "  <span class='pull-right'>\n";
-          print "    <input id='save_submit' name='submit' type='number' value='0' style='display:none'>\n"
-          print "    <button onclick='save_grading()' class='btn'>Save</button>\n";
-          print "    <button onclick='submit_grading()' class='btn btn-success'>Submit</button>\n";
-          print "    <button id='submit_btn' type='submit' style='display: none'></button>\n";
+          print "    <button id='submit_btn' type='submit' class='btn btn-success'>Submit</button>\n";
           print "  </span>\n";
           print "</div>\n";
-        }
+
         ?>
       </form>
     </div>
@@ -508,24 +447,5 @@ legend + .qtitle:nth-of-type(1) {
     <p class="pull-right"><a href="#">Back to top</a></p>
   </div>
 </footer>
-<script type="text/javascript">
-  function load_student_quiz() {
-    var class_code = document.getElementById("class_code").value;
-    var quiz_name = document.getElementById("quiz_name").value;
-    var student_name = document.getElementById("loadStudentQuiz").value;
-
-    window.location.href = "grading?class_code=" + class_code + "&quiz_name=" + quiz_name + "&student_name=" + student_name; 
-  }
-
-  function save_grading() {
-    document.getElementById('save_submit').value = 0;
-    document.getElementById('submit_btn').click();
-  }
-
-  function submit_grading() {
-    document.getElementById('save_submit').value = 1;
-    document.getElementById('submit_btn').click();
-  }
-</script>
 </body>
 </html>
