cc0ef1672d1447f85bd28647cc9a901e36967f8a
carter213 <passcut213@gmail.com>
2013-09-01 19:13:57 -0700

    change logout in quiz maker

--- quizmaker.php
+++ quizmaker.php
@@ -117,7 +117,7 @@ $quizzes = mysqli_query($con, "SELECT * FROM quiz NATURAL JOIN class WHERE
   <div class="navbar-inner">
     <div class="container">
       <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-      <a class="brand" href="./index">Logout</a>
+      <a class="brand" href="./logout">Logout</a>
       <div class="nav-collapse collapse">
         <ul class="nav">
           <li> <a href="./index">Home</a> </li>
